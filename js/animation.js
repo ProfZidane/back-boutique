e3a59750b9a3c39a006640be1f1e5e685d310d66
ProfZidane <madazada0@gmail.com>
2020-10-19 08:05:41 +0000

    ajustment couleur + espace

--- js/animation.js
+++ js/animation.js
@@ -16,6 +16,25 @@ $(document).ready(function(){
                 
                 };
         });
+
+        $(window).on('scroll', function () {
+    
+            var elmt   = $('.from-left1, .from-right1');
+            var topImg = $('.from-left1, .from-right1').offset().top;
+            var scroll = $(window).scrollTop();
+            
+                $(elmt).each(function() {
+                    
+                    var topImg = $(this).offset().top - 300;
+                    
+                        if ( topImg < scroll ) {
+                        
+                        $(this).css("transform", "translate(0,0)");
+                        $(this).css("opacity", "1");
+                        
+                        };
+                });
+            });
 });
 
  
